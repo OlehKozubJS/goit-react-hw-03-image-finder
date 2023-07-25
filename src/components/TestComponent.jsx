77851ac7445e4a5b0a5ee2a028995e829de737b5
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-25 13:41:43 +0300

    Update TestComponent.jsx

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -2,12 +2,25 @@ import { Component } from 'react';
 import propTypes from 'prop-types';
 
 export class TestComponent extends Component {
+  state = {
+    sum: 0,
+  };
+
+  reckonSum = event => {
+    return event.currenTarget.value
+      .split(' ')
+      .reduce(
+        (accumulator, number) => accumulator + Number(number),
+        this.state.sum
+      );
+  };
+
   render() {
     return (
       <div>
         <input type="text" />
-        <button>Enter</button>
-        <div></div>
+        <button onClick={this.reckonSum}>Enter</button>
+        <div>{this.state.sum}</div>
       </div>
     );
   }
