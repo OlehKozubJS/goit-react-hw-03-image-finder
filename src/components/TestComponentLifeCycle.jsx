0aa9b9327edd7c518d81cfdec321ba8d264fbc3d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-25 14:58:02 +0300

    Update TestComponentLifeCycle.jsx

--- src/components/TestComponentLifeCycle.jsx
+++ src/components/TestComponentLifeCycle.jsx
@@ -1,9 +1,25 @@
 import { Component } from 'react';
 
 export class LifeCycle extends Component {
-  state = {};
+  state = {
+    todos: [],
+    filter: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.todos) {
+    }
+  }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <form onSubmit={this.reckonSum}>
+          <input name="numbersInput" type="text" />
+          <button type="submit">Enter</button>
+        </form>
+        <div>{this.state.sum}</div>
+      </div>
+    );
   }
 }
