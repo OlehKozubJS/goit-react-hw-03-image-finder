9375352722c4eafdd3981d9381ad1536d70e55d8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-25 13:27:36 +0300

    Create TestComponent.jsx

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import propTypes from 'prop-types';
+
+export class TestComponent extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <button>Enter</button>
+        <div></div>
+      </div>
+    );
+  }
+}
