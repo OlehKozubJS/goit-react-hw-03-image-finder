af5e31e7ab66fb372d73160d89c7d4a924e7e802
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 18:45:17 +0300

    Update ImageFinder.jsx

--- src/components/ImageFinder.jsx
+++ src/components/ImageFinder.jsx
@@ -1,10 +1,14 @@
 import { Component } from 'react';
+import { Searchbar } from './Searchbar';
+import { ImageGallery } from './ImageGallery';
 
 export class ImageFinder extends Component {
-
-    render() {
-        return(
-            
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Searchbar />
+        <ImageGallery />
+      </div>
+    );
+  }
 }
