bf08de9a28e5598ee2981d9c76bcf95a33ebd70e
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 18:32:54 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,8 @@
-export const Modal = ({ imageLink, imageTitle }) => {
+export const Modal = ({ imageLink }) => {
   return (
     <div class="overlay">
       <div class="modal">
-        <img src={imageLink} alt={imageTitle} />
+        <img src={imageLink} alt="Image" />
       </div>
     </div>
   );
