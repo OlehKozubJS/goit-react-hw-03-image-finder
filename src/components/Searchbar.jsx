f6c4bab09ca9b47b23a0a48969a9c315ed1b35e4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 14:41:01 +0300

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,23 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 export class Searchbar extends Component {
-    render() {
-        return();
-    }
+  render() {
+    return (
+      <header class="searchbar">
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
 }
