884072567a0e01221d514332d212b318dce72e84
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 15:15:46 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
-export const Loader = () => {
+export const Loader = ({ isLoading }) => {
   return (
-    <div>
+    <div className={isLoading ? css.visible : css.hidden}>
       <InfinitySpin width="200" color="#4fa94d" />
     </div>
   );
