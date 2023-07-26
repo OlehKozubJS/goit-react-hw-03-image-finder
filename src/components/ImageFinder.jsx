c8262ff09ad5b8740aca7dac15bee8990c8d46b7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 20:23:37 +0300

    Update ImageFinder.jsx

--- src/components/ImageFinder.jsx
+++ src/components/ImageFinder.jsx
@@ -16,7 +16,7 @@ export class ImageFinder extends Component {
 
       try {
         const articles = await axios.get();
-        this.setState({ data: articles.filter(article => article.title) });
+        this.setState({ data: articles.filter(article => !!article.title) });
       } catch (error) {
         this.setState({ isError: true, error });
       } finally {
