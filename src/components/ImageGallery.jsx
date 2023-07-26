5877e7ca015adac32cc93421aa4da4ca5fcee9c0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 18:18:02 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,3 +1,14 @@
+import { ImageGalleryItem } from './ImageGalleryItem';
+
 export const ImageGallery = ({ imageGalleryItems }) => {
-  return <ul class="gallery"></ul>;
+  return (
+    <ul class="gallery">
+      {imageGalleryItems.map(imageGalleryItem => (
+        <ImageGalleryItem
+          imageId={imageGalleryItem.id}
+          imageLink={imageGalleryItem.webformatURL}
+        />
+      ))}
+    </ul>
+  );
 };
