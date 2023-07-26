936cf588842012549cd4d43dd8be611b216745a4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 19:41:20 +0300

    Update ImageFinder.jsx

--- src/components/ImageFinder.jsx
+++ src/components/ImageFinder.jsx
@@ -4,6 +4,15 @@ import { ImageGallery } from './ImageGallery';
 import { Button } from './Button';
 
 export class ImageFinder extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.page !== prevState.page ||
+      this.state.query !== prevState.query
+    ) {
+      fetch();
+    }
+  }
+
   render() {
     return (
       <div>
