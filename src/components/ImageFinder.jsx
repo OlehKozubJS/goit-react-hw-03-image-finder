36410cc717ea0c5674208a2deac0918f54e1ffdb
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 22:25:06 +0300

    Update ImageFinder.jsx

--- src/components/ImageFinder.jsx
+++ src/components/ImageFinder.jsx
@@ -20,6 +20,8 @@ export class ImageFinder extends Component {
         const articles = await axios.get(
           'https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12'
         );
+        articles = await articles.data;
+        console.log(articles);
         this.setState({ data: articles.filter(article => !!article.title) });
       } catch (error) {
         this.setState({ isError: true, error });
