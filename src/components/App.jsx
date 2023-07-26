2635f73d5c8cf8eb86add4af47401cb7566d41d8
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-26 19:25:01 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { ImageFinder } from './ImageFinder';
+
 export const App = () => {
   return (
     <div
@@ -8,6 +10,8 @@ export const App = () => {
         fontSize: 40,
         color: '#010101',
       }}
-    ></div>
+    >
+      <ImageFinder />
+    </div>
   );
 };
