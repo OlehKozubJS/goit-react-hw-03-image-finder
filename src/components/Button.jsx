1a524277cb64cb45c928169a8cad621e56ee5950
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 17:59:23 +0300

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,12 @@
 import css from './styles/styles.module.css';
+import ButtonCSS from './styles/Button.module.css';
 
 export const Button = ({ isLoadMore, clickFunction }) => {
   return (
     <div className={isLoadMore ? css.visible : css.hidden}>
-      <button onClick={clickFunction}>Load more</button>
+      <button onClick={clickFunction} className={ButtonCSS.Button}>
+        Load more
+      </button>
     </div>
   );
 };
