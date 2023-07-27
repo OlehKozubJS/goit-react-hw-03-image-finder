e23ae3a62e2482b6c5fed3550d274a81a3a7bbd0
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 23:55:03 +0300

    Update fetchImages.js

--- src/components/js/fetchImages.js
+++ src/components/js/fetchImages.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-export async function fetchImages(searchResult) {
+export async function fetchImages(searchResult, pageNumber) {
   const searchParams = new URLSearchParams({
     key: '37447910-ed3fb6b843fd00e4ff71a16f5',
     q: searchResult,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: 1,
+    page: pageNumber,
     per_page: 12,
   });
 
