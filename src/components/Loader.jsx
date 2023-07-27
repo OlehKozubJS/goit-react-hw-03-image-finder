9e134ae9bdb93bcb2d278c4350313b7d8d8b8c2f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 16:01:36 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 export const Loader = () => {
   return (
     <div>
-      <InfinitySpin width="200" color="#4fa94d" />
+      <InfinitySpin width="200" color="#3f51b5" />
     </div>
   );
 };
