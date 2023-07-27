1b313961b3401d4477a09a01bdc93ff5eba0e620
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 20:02:01 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
 import ModalCSS from './styles/Modal.module.css';
 
-export const Modal = ({ clickFunction, imageLink }) => {
+export const Modal = ({ clickFunction, keyDownFunction, imageLink }) => {
   return (
     <div
       /*className={isModal ? css.visible : css.hidden}*/
       onClick={clickFunction}
+      onKeyDown={keyDownFunction}
       className={ModalCSS.Overlay}
     >
       <div className={ModalCSS.Modal}>
