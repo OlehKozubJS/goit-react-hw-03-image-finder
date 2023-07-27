3485c89c132d50bf8741258ff42438bf4a9dd565
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 16:09:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
 import { ImageFinder } from './ImageFinder';
-
+import css from './styles/ImageFinder.module.css';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <div className={css.App}>
       <ImageFinder />
     </div>
   );
