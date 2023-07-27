04665bbcea9ba37fd742285ba58fc6adb2de900d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-28 01:03:24 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,13 @@ import { InfinitySpin } from 'react-loader-spinner';
 
 export const Loader = () => {
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <InfinitySpin width="300" color="#3f51b5" />
     </div>
   );
