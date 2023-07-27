dcbb12f2c2a2db577e19c109991d435993f49bf2
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-27 18:30:53 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,13 +1,14 @@
 import { ImageGalleryItem } from './ImageGalleryItem';
 import ImageGalleryCSS from './styles/ImageGallery.module.css';
 
-export const ImageGallery = ({ imageGalleryItems }) => {
+export const ImageGallery = ({ imageGalleryItems, itemClickFunction }) => {
   return (
     <ul className={ImageGalleryCSS.ImageGallery}>
       {imageGalleryItems.map(imageGalleryItem => (
         <ImageGalleryItem
           key={imageGalleryItem.id}
           imageLink={imageGalleryItem.webformatURL}
+          clickFunction={itemClickFunction}
         />
       ))}
     </ul>
