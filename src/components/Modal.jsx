4a728957efd07f9d8b0ab2d988c7d608f0dd3e79
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-28 18:43:40 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,31 @@
+import { Component } from 'react';
 import ModalCSS from './styles/Modal.module.css';
 
-export const Modal = ({ clickFunction, imageLink }) => {
-  return (
-    <div
-      /*className={isModal ? css.visible : css.hidden}*/
-      onClick={clickFunction}
-      className={ModalCSS.Overlay}
-    >
-      <div className={ModalCSS.Modal}>
-        <img src={imageLink} alt="" />
+export class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.closeModal.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.closeModal.bind(this));
+  }
+
+  closeModal = event => {
+    if (
+      event.type === 'click' ||
+      (event.type === 'keydown' && event.key === 'Escape')
+    ) {
+      this.setState({ isModal: false });
+    }
+  };
+
+  render() {
+    return (
+      <div onClick={this.props.clickFunction} className={ModalCSS.Overlay}>
+        <div className={ModalCSS.Modal}>
+          <img src={imageLink} alt="" />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
